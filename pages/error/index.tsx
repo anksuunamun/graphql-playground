958bc97d7502e963a058f63fcfa3f372b277f56f
anksuunamun <wgorelova@gmail.com>
2024-03-02 23:31:42 +0300

    feat: add pages templates (no private routes)

---
+++ pages/error/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ErrorPage = () => {
+  return (
+    <div>Errrror</div>
+  );
+};
+
+export default ErrorPage;
---
+++ pages/welcome/welcome.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const WelcomePage = () => {
+  return (
+    <div style={{color: 'red'}}>Welcome Page</div>
+  );
+};
+
+export default WelcomePage;
